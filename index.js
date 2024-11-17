d9848d8f18b5f22445436429cec638c1aa74f6c8
git4roman <romanforgit@gmail.com>
2024-11-17 12:47:03 +0545

    First Message

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 const port = 5000;
 
 app.use(cors());
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 let dataStore = [
   { id: 1, name: "John Doe" },
